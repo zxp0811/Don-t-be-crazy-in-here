f801e8170794cf67d6813e641e1c27e7d1c36f86
zxp0811 <138480214@qq.com>
2024-03-13 20:44:22 +0800

    init commit

---
+++ src/main/java/com/zxp/dontbecrazyinhere/DonTBeCrazyInHereApplication.java
@@ -0,0 +1,13 @@
+package com.zxp.dontbecrazyinhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonTBeCrazyInHereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonTBeCrazyInHereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxp/dontbecrazyinhere/DonTBeCrazyInHereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxp.dontbecrazyinhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonTBeCrazyInHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
